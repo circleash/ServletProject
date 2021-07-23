04948347633af72c53c0295d2505f699687b2098
DESKTOP-4GKAT56\JE <circleash92@gmail.com>
2021-07-23 15:39:16 +0900

    [최원제] - json 깔끔하게 변경

--- src/main/java/com/circleash/servlet/ServletTest06.java
+++ src/main/java/com/circleash/servlet/ServletTest06.java
@@ -24,6 +24,17 @@ public class ServletTest06 extends HttpServlet {
 		// {"addition : " + (number1 + number2), "subtraction : " + (number1 - number2), 
 		// "multiplication : " + (number1 * number2), "division : " + (number1 // number2)}
 		
-		out.println("{\"addition\":\""  + (number1 + number2) + "\", \"substraction\":\"" + (number1 - number2) + "\", \"multiplication\" :\"" + (number1 * number2) + "\", \"division\" :\"" + (number1 / number2) + "\" }");
+//		#뭐가 닫고 뭐가 열고의 의미는 없ㅇ어요
+//		" 는 문자열을 묶기 위한거고요
+//		\" 는 따옴표 안에 들어 가는 따옴표라는 것이고
+//		딱 요 두가지 입니다
+
+		
+		out.println("{");
+		out.println("\"addition\":"  + (number1 + number2) + ",");
+		out.println("\"substraction\":" + (number1 - number2) + ",");
+		out.println("\"multiplication\":" + (number1 * number2) + ",");
+		out.println("\"division\":" + (number1 / number2));
+		out.println("}");
 	}
 }
